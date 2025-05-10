90d613dfade131231be2c3e577c72fe155f3f261
Santhosh796373 <sandysanthosh785@gmail.com>
2025-05-10 15:00:33 +0530

    useRef hook for focus change

--- project1/src/AddItem.js
+++ project1/src/AddItem.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { FaPlus } from 'react-icons/fa6'
 
 const AddItem = ({newItem,setnewItem,handleSumbit}) => {
+
+  const inputref = useRef()
+
   return (
     <form className='addForm' onSubmit={handleSumbit}>
         <label htmlFor="addItem">AddItem</label>
         <input 
         autoFocus
+        ref={inputref}
         type="text"
         placeholder='Add Item'
         id="addItem" 
@@ -16,6 +20,7 @@ const AddItem = ({newItem,setnewItem,handleSumbit}) => {
         <button
         type='sumbit'
         aria-label='Add Item'
+        onClick={()=>inputref.current.focus()}
         >
             <FaPlus/>
 
